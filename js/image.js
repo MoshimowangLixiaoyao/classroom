7c5168cf94b281213b65eaed51ef48fea0793dfa
莫失莫忘李逍遥 <15105910413@163.com>
2024-06-06 00:32:28 +0800

    layout

---
+++ js/image.js
@@ -0,0 +1,8 @@
+// var bannerToBottom = document.getElementById('bannerToBottom')
+// bannerToBottom.style.objectFit = 'contain'
+// bannerToBottom.style.objectPosition = 'top'
+
+// console.log(bannerToBottom.style)
+
+// object-fit: contain;
+// object-position: top;
